04929314dd6b8d548e69c9f57b8e2165fda0505d
Steven Ding <anluoridge@gmail.com>
2019-04-25 01:37:19 +1000

    replace React hello world with Yarn's

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://yarnpkg.com/assets/feature-speed.png" className="App-logo" alt="logo" />
         <p>
           Hello Yarn! I'm from npm.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
